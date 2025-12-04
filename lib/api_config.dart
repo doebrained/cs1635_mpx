09ecd05f05397284ae696a8cbfe8dd472b200fd2
amandacot <67836328+amandacot@users.noreply.github.com>
2025-12-04 15:18:30 -0500

    Update api_config.dart

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,1 +1,9 @@
+// lib/api_config.dart
 
+// DO NOT COMMIT THIS FILE (it's already in .gitignore).
+
+class ApiConfig {
+  static const String spoonacularApiKey = '1e698266625c410c93d84735a7f5cd85';
+
+  static const String spoonacularBaseUrl = 'https://api.spoonacular.com';
+}
